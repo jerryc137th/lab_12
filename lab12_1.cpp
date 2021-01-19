c20aedeb68cb789264873a432fb84ad92a288740
jerryc137th <76035472+jerryc137th@users.noreply.github.com>
2021-01-19 20:41:17 +0700

    ;-;

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,22 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+void stat(const double A[],int N,double B[]){
+	double sum,sum_sd,max = A[0],min = A[0],gm =1,hm = 0;
+	for(int i = 0;i<N;i++){
+		sum += A[i];
+		sum_sd += pow(A[i],2);
+		gm *= A[i];
+		hm += (1/A[i]);
+		if(A[i]>max) max = A[i];
+		if(A[i]<min) min = A[i];	
+		}
+	B[0] = sum/N;	
+	B[1] = sqrt((sum_sd/N)-pow(B[0],2));
+	B[2] = pow(gm,(1.0/N));
+	B[3] = N/(hm);
+	B[4] = max;
+	B[5] = min;	
+}
